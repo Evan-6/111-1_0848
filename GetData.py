7e037dc431f1dd4e5ee0d0ed425b64ba6c5d3535
jamesD33064 <james0120160379@gmail.com>
2022-12-12 22:47:08 +0800

    小改

--- GetData.py
+++ GetData.py
@@ -60,12 +60,8 @@ if __name__ == '__main__':
     url = "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=4051&limited=TWN"
     response = requests.get(url)
     data = json.loads(response.text)
-    # print(data)
-    url = "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=4051&limited=TWN"
-    response = requests.get(url)
-    data = json.loads(response.text)
     month = '01'
-    numsum = 0
+    total = 0
     plot_numlist=[]
     for i in data:
         if month=='11':
@@ -73,12 +69,12 @@ if __name__ == '__main__':
         data_month = i.get('a04').split('-')[1]
         data_num = i.get('a06')
         if month==data_month:
-            numsum += int(data_num)
+            total += int(data_num)
         else:
             # print(month , numsum)
-            plot_numlist.append(numsum)
+            plot_numlist.append(total)
             month = data_month
-            numsum = 0
+            total = 0
     print(plot_numlist)
 
 
